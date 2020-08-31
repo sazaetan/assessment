5c65ecef07d56602fede4681cf60895e2394aa09
sazaetan <65839334+sazaetan@users.noreply.github.com>
2020-09-01 01:27:24 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -49,6 +49,7 @@ assessmentButton.onclick = () => {
   tweetDivided.appendChild(script);
 };
 
+'use strict';
 const answers = [
   '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
